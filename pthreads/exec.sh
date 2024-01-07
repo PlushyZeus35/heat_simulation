88d93de4436694f3c454c5ac09369ec8bd710ca5
plushyzeus <plushyzeus35@gmail.com>
2024-01-07 19:59:21 +0100

    pthreads execution file

---
+++ pthreads/exec.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+THREADS=4
+EXECUTIONFILE="simulation"
+TEMPDIRECTORY="temp"
+
+if [ "$#" -ge 1 ]; then
+	THREADS=$1
+fi
+
+# Verificar si la carpeta 'temp' existe
+if [ -d "$TEMPDIRECTORY" ]; then
+    # Borrar la carpeta 'temp' y su contenido
+    rm -r "$TEMPDIRECTORY"
+    echo "Carpeta $TEMPDIRECTORY y su contenido eliminados."
+fi
+
+mkdir "$TEMPDIRECTORY"
+echo "Carpeta $TEMPDIRECTORY creada de nuevo."
+
+make clean
+make
+./$EXECUTIONFILE $THREADS
