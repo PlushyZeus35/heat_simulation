967647639e234b4c3b7f852e6b249915cfe88923
plushyzeus <plushyzeus35@gmail.com>
2024-01-08 18:32:00 +0100

    fix logs

--- pthreads/exec.sh
+++ pthreads/exec.sh
@@ -2,8 +2,6 @@
 THREADS=4
 EXECUTIONFILE="simulation"
 TEMPDIRECTORY="temp"
-PYTHONCMD="python"
-PYTHONSCRIPT="doGif.py"
 
 if [ "$#" -ge 1 ]; then
 	THREADS=$1
@@ -23,16 +21,3 @@ make clean
 make
 ./$EXECUTIONFILE $THREADS
 
-echo "Intentando generar el gif."
-if command -v python3 &>/dev/null; then
-    PYTHONCMD=python3
-elif command -v python &>/dev/null; then
-    PYTHONCMD=python
-else
-    echo "No se encontró Python en el sistema."
-    exit 1
-fi
-
-echo "Ejecutando el script con $PYTHONCMD"
-
-$PYTHONCMD $PYTHONSCRIPT 
